8348c22d397a9707fd918df3cc7607e5fb397ac2
Silkie Agarwal <silkieagarwal261001@gmail.com>
2023-03-10 20:19:05 +0530

    3

--- discord-backend/server.js
+++ discord-backend/server.js
@@ -4,14 +4,27 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const authRoutes = require('./routes/authRoutes');
+
 const PORT = process.env.PORT || process.env.API_PORT;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+//register the routes
+app.use('/api/auth', authRoutes);
+
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is listening on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('Database connection failed. Server not started');
+    console.error(err);
+  });
