cf8b6e9edfc1f5096324cb5556aac77099cb4524
Silkie Agarwal <silkieagarwal261001@gmail.com>
2023-03-11 12:45:37 +0530

    5

--- discord-backend/controllers/auth/postLogin.js
+++ discord-backend/controllers/auth/postLogin.js
@@ -1,5 +1,30 @@
-const postLogin = (req, res) => {
-  res.send('login route');
+const User = require('../../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const postLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email.toLowerCase() });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      //send new token
+      const token = 'JWT_TOKEN';
+
+      return res.status(200).json({
+        userDetails: {
+          email: user.email,
+          token: token,
+          username: user.username,
+        },
+      });
+    }
+
+    return res.status(400).send('Invalid credentials. Please try again.');
+  } catch (err) {
+    return res.status(500).send('Something went wrong. Please try again');
+  }
 };
 
 module.exports = postLogin;
